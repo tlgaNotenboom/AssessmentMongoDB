232c42ffa712bf214193da8e04f92a5046013f47
Thomas Notenboom <tlganote@avans.nl>
2018-11-26 15:22:45 +0100

    Required Thread Schema

--- src/models/thread.js
+++ src/models/thread.js
@@ -2,8 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ThreadSchema = new Schema ({
-    title: String,
-    content: String,
+    username: {
+        type: String,
+        required: [true, 'Username is required.']
+    },
+    title: {
+        type: String,
+        required: [true, 'Title is required.']
+    },
+    content: {
+        type: String,
+        required: [true, 'Content is required.']
+    },
     comments: [{ type: Schema.Types.ObjectId, 
         ref: 'comment'}]
     
