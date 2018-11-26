4c5020b95d49b27c3b62663d9e24870ed5f15601
Tim Welter <t.welter@avans.nl>
2018-11-26 14:34:20 +0100

    User functionality added

--- server.js
+++ server.js
@@ -2,25 +2,32 @@ const Mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const express = require('express')
 const ApiError = require('./src/ApiError')
+const user_routes = require('./src/routes/user.routes')
+const morgan = require('morgan')
+
 
 Mongoose.connect("mongodb://localhost/StudditMongo");
 Mongoose.connection
     .once('open', () => console.log("Mongoose: connection open"))
     .on("error", (err) => console.warn("Error", err));
-
+	
 const port = process.env.PORT || 3000
 
 let app = express();
 
 app.use(bodyParser.json())
 
+app.use(morgan("dev"));
+
+
 app.use("*", function(req, res, next) {
     next();
 });
 
-// ROUTES
 
-// ROUTES
+app.use('/api', user_routes);
+
+
 
 app.use('*', (req, res, next) => {
 	console.log('Non-existing endpoint');
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,130 @@
+const ApiError = require('../ApiError')
+const User = require('../models/User');
+
+module.exports = {
+    addUser(req, res, next) {
+
+        try {
+            const userProps = req.body
+            User.find({
+                name: userProps.name
+            }).then((foundUser) => {
+                if (foundUser.length === 0) {
+                    User.create(userProps).then(user => {
+                        res.status(200).send(user)
+                    }).catch((err) => {
+                        next(new ApiError(err.toString(), 400))
+                    })
+                } else {
+                    next(new ApiError("User already exists", 409))
+                }
+            })
+        } catch (ex) {
+            const error = new ApiError(ex.message || ex.toString, ex.code);
+            next(error);
+            return;
+        }
+    },
+
+    getAllUsers(req, res, next) {
+        try {
+            User.find({}, (err, users) => {
+                let userMap = {};
+
+                users.forEach(user => {
+                    userMap[user._id] = user;
+                });
+                if (userMap.length === 0) {
+                    res.status(200).send(userMap);
+                } else {
+                    next(new ApiError("No users found", 404));
+                }
+            });
+        } catch (ex) {
+            const error = new ApiError(ex.message || ex.toString, ex.code);
+            next(error);
+            return;
+        }
+    },
+
+    getSpecificUser(req, res, next) {
+        const userId = req.params.id;
+        try {
+            User.findById({
+                _id: userId
+            }, (err, users) => {
+                if (users) {
+                    res.status(200).send(users);
+                } else {
+                    next(new ApiError("No users found", 404));
+                }
+            });
+        } catch (ex) {
+            const error = new ApiError(ex.message || ex.toString, ex.code);
+            next(error);
+            return;
+        }
+    },
+
+    changePassword(req, res, next) {
+        const userProps = req.body;
+
+        try {
+            User.find({
+                name: userProps.name
+            }).then((foundUser) => {
+                if (userProps.newPassword === undefined) {
+                    next(new ApiError("Please enter a new password", 422));
+                } else if (foundUser.length === 0) {
+                    next(new ApiError("User not found", 404));
+                } else if (foundUser[0].password === userProps.password) {
+                    User.findOneAndUpdate({name: userProps.name}, {$set:{password: userProps.newPassword}}).then((editedUser) => {
+                        editedUser.password = userProps.newPassword
+                        res.status(200).send(editedUser)
+                    }).catch((err) => {
+                        next(new ApiError(err.toString(), 400))
+                    })
+                } else {
+                    next(new ApiError("Entered wrong password!", 401))
+                }
+            }).catch((err) => {
+                next(new ApiError(err.toString(), 400))
+            })
+
+
+        } catch (ex) {
+            const error = new ApiError(ex.message || ex.toString, ex.code);
+            next(error);
+            return;
+        }
+    },
+
+    removeUser(req, res, next) {
+        const userProps = req.body
+
+        try {
+            User.find({
+                name: userProps.name
+            }).then((foundUser) => {
+                if (foundUser.length === 0) {
+                    next(new ApiError("User not found", 404));
+                } else if (foundUser[0].password === userProps.password) {
+                    User.findOneAndDelete({name: userProps.name}).then((deletedUser) => {
+                        res.status(200).send({done: "User " + deletedUser.name + " deleted!" })
+                    }).catch((err) => {
+                        next(new ApiError(err.toString(), 400))
+                    })
+                } else {
+                    next(new ApiError("Entered wrong password!", 401))
+                }
+            }).catch((err) => {
+                next(new ApiError(err.toString(), 400))
+            })
+
+        } catch (ex) {
+            const error = new ApiError(ex.message || ex.toString, ex.code);
+            next(error);
+            return;
+        }
+    }
+}
