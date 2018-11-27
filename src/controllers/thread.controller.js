9c44bbd3f0210f983b30b5c7621e29c4793b2dab
Tim Welter <t.welter@avans.nl>
2018-11-27 12:02:11 +0100

    added populating

--- src/controllers/thread.controller.js
+++ src/controllers/thread.controller.js
@@ -1,11 +1,11 @@
-const Thread = require ('../models/thread')
+const Thread = require('../models/thread')
 const ApiError = require('../ApiError');
 
 module.exports = {
 
-    getAllThreads(req, res, next){
+    getAllThreads(req, res, next) {
         try {
-            Thread.find({}, (err, threads) => {
+            Thread.find({}).select("-comments").exec().then((threads) => {
                 if (threads.length !== 0) {
                     res.status(200).send(threads);
                 } else {
@@ -18,19 +18,19 @@ module.exports = {
             return;
         }
     },
-    
+
     getSpecificThread(req, res, next) {
         const threadId = req.params.id;
         try {
             Thread.findById({
                 _id: threadId
-            }, (err, threads) => {
-                if (threads) {
+            }).select().populate("comments").exec().then((threads) => {
+                if (threads.length !== 0) {
                     res.status(200).send(threads);
                 } else {
                     next(new ApiError("No thread with that ID found", 404));
                 }
-            });
+            })
         } catch (ex) {
             const error = new ApiError(ex.message || ex.toString, ex.code);
             next(error);
@@ -70,7 +70,14 @@ module.exports = {
                 if (foundThread.length === 0) {
                     next(new ApiError("Thread not found", 404));
                 } else if (foundThread.title === threadProps.title) {
-                    Thread.findOneAndUpdate({_id: req.params.id}, {$set:{content: threadProps.content, username: threadProps.username}}).then((editedThread) => {
+                    Thread.findOneAndUpdate({
+                        _id: req.params.id
+                    }, {
+                        $set: {
+                            content: threadProps.content,
+                            username: threadProps.username
+                        }
+                    }).then((editedThread) => {
                         editedThread.content = threadProps.content;
                         editedThread.username = threadProps.username;
                         res.status(200).send(editedThread)
@@ -93,7 +100,7 @@ module.exports = {
     },
 
     removeThread(req, res, next) {
-         const threadProps = req.body
+        const threadProps = req.body
 
         try {
             Thread.find({
