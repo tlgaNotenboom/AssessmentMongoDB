df188e66f70b70c8b353e201153268dfd831b4ac
Thomas Notenboom <tlganote@avans.nl>
2018-11-29 14:37:41 +0100

    Thread now shows upvotes, downvotes and karma

--- src/models/thread.js
+++ src/models/thread.js
@@ -26,10 +26,43 @@ const ThreadSchema = new Schema ({
         type: Schema.Types.ObjectId, 
         ref: 'comment',
         autopopulate: true
-    }]
+    }],
     
-});
+    },
+    {
+        toObject: {
+            virtuals: true
+        },
+        toJSON: {
+            virtuals: true 
+        }
+    });
 
+ThreadSchema.virtual('Upvotes').get(function(){
+    if(this.upvotes === undefined){
+        return 0
+    }
+    return this.upvotes.length
+});
+ThreadSchema.virtual('Downvotes').get(function(){
+    if(this.downvotes === undefined){
+        return 0
+    }
+    return this.downvotes.length
+});
+ThreadSchema.virtual('Karma').get(function(){
+    if(this.upvotes === undefined){
+        if(this.downvotes === undefined){
+            return 0
+        }else {
+            return this.downvotes.length
+        }
+    }else if(this.downvotes === undefined){
+        return this.upvotes.length
+    }else{
+        return this.upvotes.length - this.downvotes.length
+    }
+});
 ThreadSchema.plugin(require('mongoose-autopopulate'))
 const Thread = mongoose.model('thread', ThreadSchema);
 
